eb4ae505a5db4029592f38abecd377d5a8d5094e
H0NEP0T-466 <fa-23-bscs-466@lgu.edu.pk>
2025-09-03 17:18:21 +0500

    projects

--- Machine Learning/Supervised ML/Decision_Trees/DT_Regression.py
+++ Machine Learning/Supervised ML/Decision_Trees/DT_Regression.py
@@ -36,7 +36,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 
-reg = DecisionTreeRegressor(max_depth=6, random_state=42)
+reg = DecisionTreeRegressor(max_depth=3, random_state=42)
 reg.fit(X_train, y_train)
 
 
